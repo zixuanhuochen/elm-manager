f4a5b66866817ab18faa57de8a4f27d5c2c4e228
zixuanhuochen <1156215144@qq.com>
2022-03-04 21:59:10 +0800

    a

---
+++ vue.config.js
@@ -0,0 +1,83 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const isProduction = process.env.NODE_ENV === 'production';
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const externals = {
+  vue: 'Vue',
+  'vue-router': 'VueRouter',
+  axios: 'axios'
+}
+
+const cdn = {
+  // 开发环境
+  dev: {
+    css: [],
+    js: []
+  },
+ // 生产环境
+  build: {
+    css: ['https://cdn.jsdelivr.net/npm/vant@2.12/lib/index.css'],
+    js: [
+      'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.min.js',
+      'https://cdn.jsdelivr.net/npm/vue-router@3.1.5/dist/vue-router.min.js',
+      'https://cdn.jsdelivr.net/npm/axios@0.19.2/dist/axios.min.js',
+    ]
+  }
+}
+  
+
+
+
+module.exports = {
+  productionSourceMap: false,
+  publicPath: './', // 静态资源路径（默认/，打包后会白屏）
+  outputDir: 'dist', // 打包后文件的目录 （默认为dist）
+  assetsDir: 'static', //  outputDir的静态资源(js、css、img、fonts)目录  默认为‘’没有单独目录js/css/img在根目录中。
+  configureWebpack: {
+    plugins:[
+      new CompressionWebpackPlugin({
+        filename: '[path].gz[query]',
+        algorithm: 'gzip',
+        // test: /\.js$|\.html$|\.json$|\.css/,
+        test: /\.js$|\.json$|\.css/,
+        threshold: 10240, // 只有大小大于该值的资源会被处理
+        minRatio: 0.8, // 只有压缩率小于这个值的资源才会被处理
+        // deleteOriginalAssets: true // 删除原文件
+      })
+    ],
+  },
+  configureWebpack: config => {
+    
+    const plugins = [];
+    if (isProduction) {
+      config.externals = externals
+      plugins.push(
+        new UglifyJsPlugin({
+          uglifyOptions: {
+            output: {
+              comments: false, // 去掉注释
+            },
+            warnings: false,
+            compress: {
+              drop_console: true,
+              drop_debugger: false,
+              pure_funcs: ['console.log']//移除console
+            }
+          }
+        })
+      )
+    }
+  },
+  chainWebpack: config => {
+    /**
+     * 添加CDN参数到htmlWebpackPlugin配置中
+     */
+    config.plugin('html').tap(args => {
+      if (isProduction) {
+        args[0].cdn = cdn.build
+      } else {
+        args[0].cdn = cdn.dev
+      }
+      return args
+    })
+  }
+  }
